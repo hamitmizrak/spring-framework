0e6c3c594914aa59cdf6e1a60d9c7752c5567bcc
hamitmizrak <hamitmizrak@gmail.com>
2022-01-04 15:48:16 +0300

    revize

--- src/main/java/com/bilgeadam/d1/aop/a0/interceptor/AraKesici.java
+++ src/main/java/com/bilgeadam/d1/aop/a0/interceptor/AraKesici.java
@@ -1,11 +1,6 @@
 package com.bilgeadam.d1.aop.a0.interceptor;
 
-import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Named;
-
 @InterceptorKesici
-@Named
-@ApplicationScoped
 public class AraKesici {
 	
 	public String araKesiciMethod(String data) {
--- src/main/java/com/bilgeadam/d1/aop/a1/stereotype/AraKesici2.java
+++ src/main/java/com/bilgeadam/d1/aop/a1/stereotype/AraKesici2.java
@@ -1,6 +1,7 @@
 package com.bilgeadam.d1.aop.a1.stereotype;
 
 @ButunAnnotations
+
 public class AraKesici2 {
 	
 	public String araKesiciMethod(String data) {
